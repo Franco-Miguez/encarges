e76e372844c01ea72e182ec8900ea359444191c3
Franco Miguez <franco.miguez98@gmail.com>
2022-01-19 15:18:35 -0300

    cambie las funciones y agrege otras para poder acceder a todas las tablas

--- conector.py
+++ conector.py
@@ -1,5 +1,8 @@
 import mysql.connector
 import producto
+import pedido
+import compra
+import cliente
 database = mysql.connector.connect(
         port= 3307,
         host= "localhost",
@@ -12,25 +15,49 @@ cursor = database.cursor(buffered=True)
 
 class Conector:
 
-    def guardar(self, producto):
-        sql = "INSERT INTO productos (nombre,descripcion,Precio,categoria) VALUES (%s,%s,%s,%s)"
+    def guardarProducto(self, producto):
+        sql = "INSERT INTO productos (nombre,descripcion,precio,categoria) VALUES (%s,%s,%s,%s)"
         datos = (producto.getNombre(), producto.getDescripcion(),
                  producto.getPrecio(), producto.getCategoria())
         cursor.execute(sql,datos)
         database.commit()
 
-    def mostrar(self):
-        cursor.execute("SELECT * FROM productos")
+    def guardarCliente(self, cliente):
+        sql = "INSERT INTO productos (celular,nombre) VALUES (%s,%s)"
+        datos = (cliente.celular, cliente.celular)
+        cursor.execute(sql,datos)
+        database.commit()
+
+    def guardarPedido(self, pedido):
+        sql = "INSERT INTO productos (fecha,entrega,informacion,seña,clientes_id) VALUES (%s,%s,%s,%s,%s)"
+        datos = (pedido.getFecha(), pedido.getEntrega(),
+                 pedido.getInformacion(), pedido.getsena(),
+                 pedido.getClientesId())
+        cursor.execute(sql,datos)
+        database.commit()
+
+    def guardarCompra(self, compra):
+        sql = "INSERT INTO productos (nombre,precio,cantidad,descripcion,pedidos_id) VALUES (%s,%s,%s,%s,%s)"
+        datos = (compra.getNombre(), compra.getPrecio(),
+                 compra.getCantidad(), compra.getCantidad,
+                 compra.getPedidosId())
+        cursor.execute(sql,datos)
+        database.commit()
+
+    def mostrar(self,tabla):
+        sql = "SELECT * FROM %s" % tabla
+        cursor.execute(sql)
         return (cursor.fetchall())
 
-    def eliminar(self,nombre):
-        sql = "DELETE FROM productos WHERE nombre = '%s' " % (nombre)
+    def eliminar(self,nombre,tabla):
+        sql = "DELETE FROM %s WHERE nombre = '%s' " % (tabla,nombre)
         cursor.execute(sql)
         database.commit()
 
-    def modificar(self,nombreProducto,columnaModificar,valorNuevo):
-        sql = "UPDATE productos SET %s = '%s' WHERE nombre = '%s'" % (columnaModificar,
-                                                                         valorNuevo,
-                                                                         nombreProducto)
+    def modificar(self,nombreProducto,columnaModificar,valorNuevo,tabla):
+        sql = "UPDATE %s SET %s = '%s' WHERE nombre = '%s'" % (tabla,
+                                                               columnaModificar,
+                                                                valorNuevo,
+                                                                nombreProducto)
         cursor.execute(sql)
         database.commit()
