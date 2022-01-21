4996f84fed799875e195b9733bff5e5e83a339c5
Franco Miguez <franco.miguez98@gmail.com>
2022-01-21 15:11:56 -0300

    Creación de ventana y barra superior de secciones

--- main.py
+++ main.py
@@ -1,46 +1,11 @@
 import producto
 import conector
+import entorno_grafico
 
 conector = conector.Conector()
 
-print("""
-Menu:
-1. Agregar producto
-2. Ver Productos
-3. Eliminar producto
-4. modificar producto
-""")
-eleccion = ""
-try:
-    while True:
-        eleccion = int(input("Ingrese un valor: "))
-        if(eleccion > 0 and eleccion < 5):
-            break
-except NameError:
-    print("Ingrese un valor numerico valido")
+programa = entorno_grafico.Programa(conector)
 
-
-if eleccion == 1:
-    nombre = input("Nombre: ")
-    descripcion = input("descripcion: ")
-    precio = float(input("precio: $"))
-    categoria = input("Categoria: ")
-    nuevoProducto = producto.Producto(nombre, descripcion, precio, categoria)
-    conector.guardarProducto(nuevoProducto)
-
-elif eleccion == 2:
-    for articulo in conector.mostrar("productos"):
-        productoAMostrar = producto.Producto(articulo[1],articulo[2],articulo[3],articulo[4])
-        print (productoAMostrar.datos())
-elif eleccion == 3:
-    print ("##### ELIMINAR PRODUCTO #####")
-    productoAEliminar = input("nombre: ")
-    conector.eliminar(productoAEliminar,"productos")
-elif eleccion == 4:
-    print ("##### MODIFICAR PRODUCTO #####")
-    for articulo in conector.mostrar("productos"):
-        print("\n- " + articulo[1])
-    nombre = input("Nombre: ")
-    columna = input("elije la seccion a modificar: ")
-    nuevoValor = input("Ingrese la nueva modificación: ")
-    conector.modificar(nombre, columna, nuevoValor,"productos")
+programa.ejecutar()
+programa.Secciones()
+programa.mostrar()
